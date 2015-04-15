3e324e8000ed1a95b6038f82c18f35225a854bad
Arturo Brotons <Arturo.Brotons@TIEKinetix.com>
2015-04-15 17:49:40 +0200

    Added human readable to Myers Briggs Indicator enum (temporary)

--- app/src/main/java/eu/alfred/personalization_manager/db_administrator/model/MyersBriggsTypeIndicator.java
+++ app/src/main/java/eu/alfred/personalization_manager/db_administrator/model/MyersBriggsTypeIndicator.java
@@ -2,21 +2,32 @@ package eu.alfred.personalization_manager.db_administrator.model;
 
 public enum MyersBriggsTypeIndicator {
 
-	ISTJ,
-	ISFJ,
-	INFJ,
-	INTJ,
-	ISTP,
-	ISFP,
-	INFP,
-	INTP,
-	ESTP,
-	ESFP,
-	ENFP,
-	ENTP,
-	ESTJ,
-	ESFJ,
-	ENFJ,
-	ENTJ,
+	ISTJ("Introversion-Sensing-Thinking-Judgment"),
+	ISFJ("Introversion-Sensing-Feeling-Judgment"),
+	INFJ("Introversion-Intuition-Feeling-Judgment"),
+	INTJ("Introversion-Intuition-Thinking-Judgment"),
+	ISTP("Introversion-Sensing-Thinking-Perception"),
+	ISFP("Introversion-Sensing-Feeling-Perception"),
+	INFP("Introversion-Intuition-Feeling-Perception"),
+	INTP("Introversion-Intuition-Thinking-Perception"),
+	ESTP("Extraversion-Sensing-Thinking-Perception"),
+	ESFP("Extraversion-Sensing-Feeling-Perception"),
+	ENFP("Extraversion-Intuition-Feeling-Perception"),
+	ENTP("Extraversion-Intuition-Thinking-Perception"),
+	ESTJ("Extraversion-Sensing-Thinking-Judgment"),
+	ESFJ("Extraversion-Sensing-Feeling-Judgment"),
+	ENFJ("Extraversion-Intuition-Feeling-Judgment"),
+	ENTJ("Extraversion-Intuition-Thinking-Judgment");
+
+    private String friendlyName;
+
+    private MyersBriggsTypeIndicator(String friendlyName) {
+        this.friendlyName = friendlyName;
+    }
+
+    @Override
+    public String toString() {
+        return friendlyName;
+    }
 	
 }
