cdf490349dc48f55fdbbeb08f9ed4b17559b4eb4
Arturo Brotons <Arturo.Brotons@TIEKinetix.com>
2015-05-07 16:59:52 +0200

    Deleted Health profile tab and unnecessary model classes

--- app/src/main/java/eu/alfred/personalization_manager/db_administrator/model/SocietyParticipationScale.java
+++
@@ -1,9 +0,0 @@
-package eu.alfred.personalization_manager.db_administrator.model;
-
-public enum SocietyParticipationScale {
-
-	ISOLATED,
-	MODERATELY_ISOLATED,
-	MODERATELY_INTEGRATED,
-	INTEGRATED,
-}
