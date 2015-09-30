5f76a658226a9154913f6393762d1fbd9839a78f
Arturo Brotons <Arturo.Brotons@TIEKinetix.com>
2015-09-30 17:37:20 +0200

    Hide access rights checkboxes for fields that are not actually shown in the Personal tab.

--- app/src/main/java/eu/alfred/personalization_manager/db_administrator/model/AttributesHelper.java
+++ app/src/main/java/eu/alfred/personalization_manager/db_administrator/model/AttributesHelper.java
@@ -2,23 +2,64 @@ package eu.alfred.personalization_manager.db_administrator.model;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AttributesHelper {
-	
-	public List<String> getUserProfileFields() {
-		
-		UserProfile userProfile = new UserProfile();
-		Class<?> c = userProfile.getClass();
-		Field[] fields = c.getDeclaredFields();
-		ArrayList<String> userProfileFields = new ArrayList<String>();
-		
-		for(Field field : fields) 			
-			userProfileFields.add(field.getName());
-		
-		return userProfileFields;
-	}
-	
-		
-	
+
+    String[] avoid = {
+
+            "id",
+            "alfedAppInstalationDate",
+            "alfredUserName",
+            "anniversaryDate",
+            "myersBriggsIndicator",
+            "socialSecurityNumber",
+            "culturalOrFamilyNeeds",
+            "healthInsurance",
+            "selfDescrPersonalityChar",
+            "interests",
+            "nextOfKin",
+            "socialMediaProfiles"/*,
+
+
+            "citizenship",
+            "dateOfBirth",
+            "educationLevel",
+            "email",
+            "employmentStatus",
+            "firstName",
+            "gender",
+            "language",
+            "lastName",
+            "maritalStatus",
+            "middleName",
+            "mobilePhone",
+            "nationality",
+            "phone",
+            "postalAddress",
+            "prefferedName",
+            "profession",
+            "residentialAddress"*/
+    };
+
+    public List<String> getUserProfileFields() {
+
+        UserProfile userProfile = new UserProfile();
+        Class<?> c = userProfile.getClass();
+        Field[] fields = c.getDeclaredFields();
+        ArrayList<String> userProfileFields = new ArrayList<String>();
+
+        List<String> avoidList = Arrays.asList(avoid);
+
+        for (Field field : fields) {
+            if (!avoidList.contains(field.getName())) {
+                userProfileFields.add(field.getName());
+            }
+        }
+
+        return userProfileFields;
+    }
+
+
 }
