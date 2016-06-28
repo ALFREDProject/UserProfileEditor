63f2d8ee00f64b2bcf4fafde849c7963b7218bdf
Peter Merz <peter.merz@tiekinetix.com>
2016-06-28 13:32:26 +0200

    member mActivity now will be checked for null within error exception handling

--- app/src/main/java/eu/alfred/personalization_manager/controller/ContactsController.java
+++ app/src/main/java/eu/alfred/personalization_manager/controller/ContactsController.java
@@ -121,7 +121,7 @@ public class ContactsController {
 		    @Override
 		    public void OnError(Exception e) {
 			    Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			    mActivity.notification(false, "Creating contact failed");
+			    if (mActivity != null) mActivity.notification(false, "Creating contact failed");
 		    }
 	    });
 
@@ -154,7 +154,7 @@ public class ContactsController {
 		            @Override
 		            public void OnError(Exception e) {
 			            Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			            mActivity.notification(false, "Update requester failed");
+			            if (mActivity != null) mActivity.notification(false, "Update requester failed");
 		            }
 	            });
 
@@ -182,8 +182,8 @@ public class ContactsController {
 		            @Override
 		            public void OnError(Exception e) {
 			            Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			            mActivity.notification(false, "Creating requester failed");
-			            mActivity.onErrorCreatingNewRequesters(e, r2);
+			            if (mActivity != null) mActivity.notification(false, "Creating requester failed");
+			            if (mActivity != null) mActivity.onErrorCreatingNewRequesters(e, r2);
 		            }
 	            });
 
@@ -274,7 +274,7 @@ public class ContactsController {
 		    @Override
 		    public void OnError(Exception e) {
 			    Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			    mActivity.notification(false, "Delete contact failed");
+			    if (mActivity != null) mActivity.notification(false, "Delete contact failed");
 		    }
 	    });
 
@@ -296,7 +296,7 @@ public class ContactsController {
 		    @Override
 		    public void OnError(Exception e) {
 			    Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			    mActivity.notification(false, "Update contact failed");
+			    if (mActivity != null) mActivity.notification(false, "Update contact failed");
 		    }
 	    });
 
@@ -335,7 +335,7 @@ public class ContactsController {
 		            @Override
 		            public void OnError(Exception e) {
 			            Log.e(TAG, e.getClass().getSimpleName() + ": " + e.getMessage());
-			            mActivity.notification(false, "Retrieve requester failed");
+			            if (mActivity != null) mActivity.notification(false, "Retrieve requester failed");
 		            }
 	            });
 
