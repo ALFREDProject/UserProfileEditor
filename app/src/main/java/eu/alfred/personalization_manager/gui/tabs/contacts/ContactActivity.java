5fbd791b9d398b7a6135a7a4add7e00cc1cad090
Peter Merz <peter.merz@tiekinetix.com>
2016-07-12 11:14:42 +0200

    bugfix in fillForm(): Gender has to be internationalized and Gender-Str has to be mapped to english enum

--- app/src/main/java/eu/alfred/personalization_manager/gui/tabs/contacts/ContactActivity.java
+++ app/src/main/java/eu/alfred/personalization_manager/gui/tabs/contacts/ContactActivity.java
@@ -415,6 +415,13 @@ public class ContactActivity extends Activity {
             RadioButton b = (RadioButton) findViewById(rgGender.getCheckedRadioButtonId());
             if (b != null) {
                 String genderStr = b.getText().toString();
+                String genderStrTmp = genderStr.toUpperCase();
+                if (genderStrTmp.equals("HOMME") || genderStrTmp.equals("MANNELIJK")) {
+                    genderStr = "MALE";
+                }
+                else if (genderStrTmp.equals("FEMME") || genderStrTmp.equals("VROUWELIJK")) {
+                    genderStr = "FEMALE";
+                }
                 Gender gender = Gender.valueOf(genderStr.toUpperCase());
                 contact.setGender(gender);
             }
